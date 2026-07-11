fa101c74341a3cb463860c0bbd7c8455d7f04542
praveenkumar <vijayakumar.p@northeastern.edu>
2026-07-10 21:01:09 -0400

    Elevator LLD with state pattern Completion

--- ElevatorSystem/src/State/DoorOpenState.java
+++ ElevatorSystem/src/State/DoorOpenState.java
@@ -3,19 +3,51 @@ package State;
 import Model.Elevator;
 import Model.Request;
 
-public class DoorOpenState implements ElevatorState{
+import java.util.Iterator;
+
+public class DoorOpenState implements ElevatorState {
     @Override
     public void handleRequest(Elevator elevator, Request request) {
-
+        elevator.getRequestList().add(request);
     }
 
     @Override
     public void move(Elevator elevator) {
+        int currentFloor = elevator.getCurrentFloor().getFloorNumber();
+        System.out.println("Elevator " + elevator.getId() + " doors open at floor " + currentFloor);
+        Iterator<Request> it = elevator.getRequestList().iterator();
+        while (it.hasNext()) {
+            Request r = it.next();
+            if (r.getDestination().getFloorNumber() == currentFloor) {
+                it.remove();
+            }
+        }
+        if (elevator.getRequestList().isEmpty()) {
+            elevator.setElevatorState(new IdleState());
+            return;
+        }
 
+        boolean hasUp = false;
+        boolean hasDown = false;
+        for (Request r : elevator.getRequestList()) {
+            int target = r.getDestination().getFloorNumber();
+            int source = r.getSource().getFloorNumber();
+            if (target > currentFloor || source > currentFloor) hasUp = true;
+            if (target < currentFloor || source < currentFloor) hasDown = true;
+        }
+
+        if (hasUp) {
+            elevator.setElevatorState(new MovingUpState());
+        } else if (hasDown) {
+            elevator.setElevatorState(new MovingDownState());
+        } else {
+            elevator.setElevatorState(new IdleState());
+        }
     }
 
     @Override
     public void display(Elevator elevator) {
-
+        System.out.println("Elevator " + elevator.getId() + " | Floor: "
+                + elevator.getCurrentFloor().getFloorNumber() + " | DOOR OPEN");
     }
 }
--- ElevatorSystem/src/State/MovingDownState.java
+++ ElevatorSystem/src/State/MovingDownState.java
@@ -6,16 +6,34 @@ import Model.Request;
 public class MovingDownState implements ElevatorState{
     @Override
     public void handleRequest(Elevator elevator, Request request) {
-
+        elevator.getRequestList().add(request);
     }
 
     @Override
     public void move(Elevator elevator) {
+        int currentFloor = elevator.getCurrentFloor().getFloorNumber();
+        elevator.getCurrentFloor().setFloorNumber(currentFloor - 1);
+        System.out.println("Elevator " + elevator.getId() + " moving down to floor " + (currentFloor - 1));
 
+        boolean shouldStop = false;
+        for (Request r : elevator.getRequestList()) {
+            if (r.getSource().getFloorNumber() == currentFloor - 1
+                    || r.getDestination().getFloorNumber() == currentFloor - 1) {
+                shouldStop = true;
+                break;
+            }
+        }
+
+        if (shouldStop) {
+            elevator.setElevatorState(new DoorOpenState());
+        } else if (elevator.getRequestList().isEmpty()) {
+            elevator.setElevatorState(new IdleState());
+        }
     }
 
     @Override
     public void display(Elevator elevator) {
-
+        System.out.println("Elevator " + elevator.getId() + " | Floor: "
+                + elevator.getCurrentFloor().getFloorNumber() + " | MOVING DOWN");
     }
 }
