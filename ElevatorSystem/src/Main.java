48a65243021f0a4420a7195633459f8839c7563a
praveenkumar <vijayakumar.p@northeastern.edu>
2026-07-10 21:25:57 -0400

    Testing code for Elevator

--- ElevatorSystem/src/Main.java
+++ ElevatorSystem/src/Main.java
@@ -1,6 +1,47 @@
+import Controller.ElevatorController;
+import Model.Elevator;
+import Model.Floor;
+import Model.Request;
+import Model.Direction;
+import State.IdleState;
+import StrategyPattern.NearestElevatorStrategy;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // create floors 0-5
+        List<Floor> floors = new ArrayList<>();
+        for (int i = 0; i <= 5; i++) {
+            floors.add(new Floor(i, false, false));
+        }
+
+        // create 2 elevators, both idle at floor 0
+        Elevator elevator1 = new Elevator(1, 10, floors.get(0), new IdleState());
+        Elevator elevator2 = new Elevator(2, 10, floors.get(0), new IdleState());
+
+        List<Elevator> elevators = new ArrayList<>();
+        elevators.add(elevator1);
+        elevators.add(elevator2);
+
+        // controller with nearest elevator strategy
+        ElevatorController controller = new ElevatorController(elevators, new StrategyPattern.NearestElevatorStrategy());
+
+        // request 1: someone on floor 2 wants to go to floor 5
+        Request req1 = new Request(floors.get(2), Direction.UP, floors.get(5));
+        controller.submitRequest(req1);
+
+        // request 2: someone on floor 4 wants to go to floor 1
+        Request req2 = new Request(floors.get(4), Direction.DOWN, floors.get(1));
+        controller.submitRequest(req2);
+
+        // simulate 10 steps
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("--- Step " + i + " ---");
+            controller.step();
+            controller.displayStatus();
+        }
     }
 }
 /*
