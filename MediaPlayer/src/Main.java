01d1525eb3ccb463e0b5a3159f9b5180cf0d179d
praveenkumar <vijayakumar.p@northeastern.edu>
2026-07-15 19:05:06 -0400

    Main code media player

--- MediaPlayer/src/Main.java
+++ MediaPlayer/src/Main.java
@@ -1,6 +1,54 @@
+import Entities.Player;
+import Entities.Song;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Song> playlist = new ArrayList<>();
+        playlist.add(new Song(1, "Shape of You", 4));
+        playlist.add(new Song(2, "Blinding Lights", 3));
+        playlist.add(new Song(3, "Bohemian Rhapsody", 6));
+        playlist.add(new Song(4, "Hotel California", 7));
+
+        Player player = new Player(1, playlist);
+
+        System.out.println("=== Initial State (Stopped) ===");
+        System.out.println("State: " + player.getCurrentState().getClass().getSimpleName());
+
+        System.out.println("\n=== Play a song from Stopped ===");
+        player.play(playlist.get(0));
+        System.out.println("State: " + player.getCurrentState().getClass().getSimpleName());
+
+        System.out.println("\n=== Pause the song ===");
+        player.pause();
+        System.out.println("State: " + player.getCurrentState().getClass().getSimpleName());
+        System.out.println("\n=== Resume/Play from Paused ===");
+        player.play(playlist.get(0));
+        System.out.println("State: " + player.getCurrentState().getClass().getSimpleName());
+
+        System.out.println("\n=== Switch track while Playing ===");
+        player.play(playlist.get(2));
+        System.out.println("State: " + player.getCurrentState().getClass().getSimpleName());
+        System.out.println("Current song index: " + player.getCurrentSongIndex());
+
+        System.out.println("\n=== Skip Forward ===");
+        player.next();
+        System.out.println("Current song index: " + player.getCurrentSongIndex());
+
+        System.out.println("\n=== Skip Backward ===");
+        player.previous();
+        System.out.println("Current song index: " + player.getCurrentSongIndex());
+        System.out.println("\n=== Stop the player ===");
+        player.stop();
+        System.out.println("State: " + player.getCurrentState().getClass().getSimpleName());
+
+        System.out.println("\n=== Try pause in Stopped state ===");
+        player.pause();
+
+        System.out.println("\n=== Try next in Stopped state ===");
+        player.next();
     }
 }
 
